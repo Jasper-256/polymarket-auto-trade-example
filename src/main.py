bc9b363a5cd7e2b572bbe11a81ce1add90c33c76
Jasper Morgal <jpmorgal8@gmail.com>
2025-01-18 00:32:04 -0800

    It is functional now

--- src/main.py
+++ src/main.py
@@ -6,33 +6,103 @@ from helpers.set_allowances import set_allowances
 from api_keys.create_api_key import generate_api_keys
 from markets.get_markets import get_market
 from trades.trade_specific_market import limit_order
+from trades.trade_specific_market import conditional_order
 from trades.trade_specific_market import update_order_info
 from trades.trade_specific_market import cancel_orders
 from trades.trade_specific_market import get_order_book
-from trades.trade_specific_market import get_best_bid
-from trades.trade_specific_market import get_best_ask
+from trades.trade_specific_market import get_best_bid_yes
+from trades.trade_specific_market import get_best_ask_yes
+from trades.trade_specific_market import get_best_bid_no
+from trades.trade_specific_market import get_best_ask_no
 from trades.trade_specific_market import get_spread
-from trades.trade_specific_market import get_midpoint
+from trades.trade_specific_market import get_midpoint_yes
+from trades.trade_specific_market import get_midpoint_no
 from trades.trade_specific_market import update_all_order_info
 from trades.trade_specific_market import remove_orders_from_file
-
+from trades.trade_specific_market import get_orders
+from trades.trade_specific_market import get_positions
+from trades.trade_specific_market import get_bands
+from trades.trade_specific_market import get_orders_in_band
+from trades.trade_specific_market import calculate_totals_in_band
 
 
 load_dotenv()
 
-def auto_make_market(market):
-    open_order_ids = []
-    order_book = get_order_book(market)
+market_to_trade = '0xb08d4306dc14f563528a231a177d7888b44e2d09274cac65331c37f53fac6bf2'
+
+def auto_make_market(market_id):
+    market = get_market(market_id)
 
     while True:
-        # Update order book
-        order_book = get_order_book(market)
+        with open('stop.txt', 'r') as file:
+            stop_file_data = file.read().rstrip()
+        if stop_file_data == 'true':
+            break
 
-        # Get current position
-        print("Cycle")
+        # Update
+        update_all_order_info()
+
+        order_book = get_order_book(market)
+        best_bid_yes = get_best_bid_yes(order_book)
+        best_ask_yes = get_best_ask_yes(order_book)
+        midpoint_yes = get_midpoint_yes(order_book)
+        best_bid_no = get_best_bid_no(order_book)
+        best_ask_no = get_best_ask_no(order_book)
+        midpoint_no = get_midpoint_no(order_book)
+        spread = get_spread(order_book)
+
+        orders = get_orders()
+        positions = get_positions()
+        bands = get_bands()
+
+        total_positions_value = positions[market_id]['yes'] * best_bid_yes + positions[market_id]['no'] * best_bid_no
+        owned_outcome = 'none'
+        owned_yes = positions[market_id]['yes'] - positions[market_id]['no']
+        owned_outcome_value = 0
+        cancelable_tokens = min(positions[market_id]['yes'], positions[market_id]['no'])
+        if owned_yes > 0:
+            owned_outcome = 'yes'
+            owned_outcome_value = owned_yes * best_bid_yes
+        elif owned_yes < 0:
+            owned_outcome = 'no'
+            owned_outcome_value = -owned_yes * best_bid_no
+        position_value = round(cancelable_tokens + owned_outcome_value, 3)
+
+        print(f'[POSITION]\towned_yes={owned_yes}, owned_outcome_value={owned_outcome_value}, cancelable_tokens={cancelable_tokens}')
+        print(f'[YES PRICES]\tbest_bid_yes={best_bid_yes}, best_ask_yes={best_ask_yes}, midpoint_yes={midpoint_yes}')
+        print(f'[NO PRICES]\tbest_bid_no={best_bid_no}, best_ask_no={best_ask_no}, midpoint_no={midpoint_no}, spread={spread}')
+
+        outside_order_ids = []
+        for order in orders:
+            outside_order_ids.append(order['order_id'])
+        
+        for band_num in range(bands['num_bands']):
+            orders_in_band = get_orders_in_band(band_num, midpoint_yes, midpoint_no)
+
+            for order in orders_in_band:
+                outside_order_ids.remove(order['order_id'])
+
+        # Cancel orders outside bands
+        if len(outside_order_ids) > 0:
+            cancel_orders(outside_order_ids)
+
+        for band_num in range(bands['num_bands']):
+            orders_in_band = get_orders_in_band(band_num, midpoint_yes, midpoint_no)
+            
+            yes_total, no_total = calculate_totals_in_band(orders_in_band)
+
+            if yes_total < bands[str(band_num)]['min_amount']:
+                conditional_order(market=market, outcome='yes', current_total=yes_total, bands=bands, band_num=band_num, midpoint_yes=midpoint_yes, midpoint_no=midpoint_no)
+            if no_total < bands[str(band_num)]['min_amount']:
+                conditional_order(market=market, outcome='no', current_total=no_total, bands=bands, band_num=band_num, midpoint_yes=midpoint_yes, midpoint_no=midpoint_no)
+            
+            print(f'[BAND {band_num}]\tyes_total={yes_total}, no_total={no_total}')
+        print()
+
+auto_make_market(market_to_trade)
 
 # Find the condition ID for the market you want to trade and retrieve market info from CLOB
-trump_aliens = get_market('0xbb96f092cb5d54138c6af2ae824bb276c3e20969fb2acfced30ac7f88f60862e')
+# trump_aliens = get_market(trump_aliens_id)
 # print(trump_aliens)
 
 # Get information about the order book
@@ -50,7 +120,7 @@ trump_aliens = get_market('0xbb96f092cb5d54138c6af2ae824bb276c3e20969fb2acfced30
 # update_order_info('0x1a28362aa37250725b400fabb0d0bd9ee9e3550a6d610a807a78ec4a1ddec50a')
 
 # Update all orders in the json
-update_all_order_info()
+# update_all_order_info()
 
 # Cancel an order
 # cancel_orders(['0xa717f659b055c8d520e593c4b85078e7bff8c7747288b8b859cb6b3268ab7504'])
@@ -68,5 +138,3 @@ update_all_order_info()
 
 # midpoint = get_midpoint(order_book)
 # print('Midpoint:', midpoint)
-
-# auto_make_market(trump_aliens)
--- src/trades/trade_specific_market.py
+++ src/trades/trade_specific_market.py
@@ -10,6 +10,7 @@ from py_clob_client.order_builder.constants import SELL
 
 import json
 import os
+import math
 
 
 def create_and_submit_order(token_id: str, side: Literal['BUY'] | Literal['SELL'], price: float, size: int):
@@ -44,7 +45,8 @@ def limit_order(market: dict, side: str, outcome: str, price: float, size: int):
     
     # Create and submit the order
     order_response = create_and_submit_order(token_id=token_id, side=side_literal, price=price, size=size)
-    print(order_response)
+
+    print(f'[ORDER PLACED]\tside={side}, outcome={outcome}, price={price}, size={size}')
 
     # If the order was successfully created, record it to the JSON file
     if order_response.get('success') and 'orderID' in order_response:
@@ -52,6 +54,122 @@ def limit_order(market: dict, side: str, outcome: str, price: float, size: int):
         record_order_to_file(order_id, market['condition_id'], side, outcome, price, size)
     else:
         print(f"Failed to create order: {order_response.get('errorMsg', 'Unknown error')}")
+
+def conditional_order(market, outcome, current_total, bands, band_num, midpoint_yes, midpoint_no):
+    if outcome == 'yes':
+        price = midpoint_yes - bands[str(band_num)]['avg_margin']
+    elif outcome == 'no':
+        price = midpoint_no - bands[str(band_num)]['avg_margin']
+    else:
+        return
+    price = round(price, 3)
+    
+    size = bands[str(band_num)]['avg_amount'] - current_total
+    if size < 5:
+        size = 5
+    if size * price < 1:
+        size = math.ceil(1 / price)
+
+    limit_order(market=market, side='buy', outcome=outcome, price=price, size=size)
+
+def get_orders(file_path: str = 'orders.json'):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            try:
+                data = json.load(file)
+            except json.JSONDecodeError:
+                data = []
+    else:
+        data = []
+    return data
+
+def get_positions(file_path: str = 'positions.json'):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            try:
+                data = json.load(file)
+            except json.JSONDecodeError:
+                data = []
+    else:
+        data = []
+    return data
+
+def get_bands(file_path: str = 'bands.json'):
+    if os.path.exists(file_path):
+        with open(file_path, 'r') as file:
+            try:
+                data = json.load(file)
+            except json.JSONDecodeError:
+                data = []
+    else:
+        data = []
+    return data
+
+def get_orders_in_band(band_num, midpoint_yes, midpoint_no, file_path='orders.json'):
+    """
+    Retrieve orders from orders.json that fall within a specific band range for yes and no outcomes.
+    """
+    bands = get_bands()
+    min_margin = bands[str(band_num)]['min_margin']
+    max_margin = bands[str(band_num)]['max_margin']
+
+    if not os.path.exists(file_path):
+        print("Orders file does not exist.")
+        return []
+
+    try:
+        with open(file_path, 'r') as file:
+            orders = json.load(file)
+    except json.JSONDecodeError:
+        print("Failed to read orders.json or it is empty.")
+        return []
+
+    filtered_orders = []
+
+    for order in orders:
+        price = order.get('price')
+        outcome = order.get('outcome')
+
+        if outcome == 'yes':
+            if (midpoint_yes - max_margin) < price <= (midpoint_yes - min_margin) or \
+               (midpoint_yes + min_margin) <= price < (midpoint_yes + max_margin):
+                filtered_orders.append(order)
+
+        elif outcome == 'no':
+            if (midpoint_no - max_margin) < price <= (midpoint_no - min_margin) or \
+               (midpoint_no + min_margin) <= price < (midpoint_no + max_margin):
+                filtered_orders.append(order)
+
+    return filtered_orders
+
+def calculate_totals_in_band(orders_in_band):
+    """
+    Calculate the total effective amount of 'yes' and 'no' contracts in a band.
+
+    Args:
+        orders_in_band (list): A list of order objects from get_orders_in_band().
+
+    Returns:
+        tuple: A tuple containing the total 'yes' and 'no' amounts (yes_total, no_total).
+    """
+    yes_total = 0
+    no_total = 0
+
+    for order in orders_in_band:
+        size_delta = order.get('size', 0) - order.get('size_matched', 0)
+        side = order.get('side')
+        outcome = order.get('outcome')
+
+        if side == 'buy' and outcome == 'yes':
+            yes_total += size_delta
+        elif side == 'buy' and outcome == 'no':
+            no_total += size_delta
+        elif side == 'sell' and outcome == 'yes':
+            no_total += size_delta
+        elif side == 'sell' and outcome == 'no':
+            yes_total += size_delta
+
+    return yes_total, no_total
 
 def record_order_to_file(order_id: str, market_id: str, side: str, outcome: str, price: float, size: int, file_path: str = 'orders.json'):
     """Record the order ID and size to an external JSON file."""
@@ -82,8 +200,6 @@ def record_order_to_file(order_id: str, market_id: str, side: str, outcome: str,
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
-    print(f"Order recorded: {order_data}")
-
 def remove_orders_from_file(order_ids, file_path: str = 'orders.json'):
     if os.path.exists(file_path):
         with open(file_path, 'r') as file:
@@ -100,8 +216,6 @@ def remove_orders_from_file(order_ids, file_path: str = 'orders.json'):
     # Write the updated data back to the file
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
-
-    print(f"Canceled orders removed from file: {order_ids}")
 
 def update_position(market_id, side, outcome, size_delta, file_path: str = 'positions.json'):
     positions = {}
@@ -128,7 +242,7 @@ def update_position(market_id, side, outcome, size_delta, file_path: str = 'posi
     with open(file_path, 'w') as file:
         json.dump(positions, file, indent=4)
 
-    print(f"Updated positions for market_id: {market_id}, side: {side}, outcome: {outcome}, size_delta: {size_delta}")
+    print(f'[ORDER FILLED]\tside={side}, outcome={outcome}, filled={size_delta}')
 
 def get_order_info(order_id):
     client = create_clob_client()
@@ -155,9 +269,13 @@ def update_order_info(order_id, file_path: str = 'orders.json'):
                 return # Don't fetch order info if it's been fully filled to save time
             
             order = get_order_info(order_id)
+            print(f'[UPDATE ORDER]\torder_id={order_id}')
             
-            new_size_matched = int(order.get('size_matched', 0))
+            new_size_matched = float(order.get('size_matched', 0))
+            price = float(order.get('price', 0))
+
             record['size_matched'] = new_size_matched
+            record['price'] = price
 
             # Check for changes in size_matched and update position
             if old_size_matched != new_size_matched:
@@ -172,7 +290,7 @@ def update_order_info(order_id, file_path: str = 'orders.json'):
     with open(file_path, 'w') as file:
         json.dump(data, file, indent=4)
 
-    print(f"Order updated: {order_id} with size_matched: {order.get('size_matched', 0)}")
+    # print(f"Order updated: {order_id} with size_matched: {order.get('size_matched', 0)}")
 
 def update_all_order_info(file_path: str = 'orders.json'):
     """Update the size_matched value for all orders in the JSON file."""
@@ -194,8 +312,10 @@ def cancel_orders(order_ids):
     client = create_clob_client()
 
     resp = client.cancel_orders(order_ids=order_ids)
+    successfully_canceled_orders = resp['canceled']
 
-    remove_orders_from_file(resp['canceled'])
+    remove_orders_from_file(order_ids)
+    print(f'[CANCELED]\torder_ids={successfully_canceled_orders}')
 
 def get_order_book(market):
     client = create_clob_client()
@@ -203,34 +323,49 @@ def get_order_book(market):
     token_id = next((item for item in market['tokens'] if item.get('outcome') == 'Yes'), None)['token_id']
     return client.get_order_book(token_id=token_id)
 
-def get_best_bid(order_book):
+def get_best_bid_yes(order_book):
     if not order_book.bids:
         return None
     
     best_bid = max(order_book.bids, key=lambda bid: float(bid.price))
     return float(best_bid.price)
 
-def get_best_ask(order_book):
+def get_best_ask_yes(order_book):
     if not order_book.asks:
         return None
     
     best_ask = min(order_book.asks, key=lambda ask: float(ask.price))
     return float(best_ask.price)
 
+def get_best_bid_no(order_book):
+    return round(1 - get_best_ask_yes(order_book), 3)
+
+def get_best_ask_no(order_book):
+    return round(1 - get_best_bid_yes(order_book), 3)
+
 def get_spread(order_book):
-    best_bid = get_best_bid(order_book)
-    best_ask = get_best_ask(order_book)
+    best_bid_yes = get_best_bid_yes(order_book)
+    best_ask_yes = get_best_ask_yes(order_book)
 
-    if not best_bid or not best_ask:
+    if not best_bid_yes or not best_ask_yes:
         return None
 
-    return round(best_ask - best_bid, 3)
+    return round(best_ask_yes - best_bid_yes, 3)
 
-def get_midpoint(order_book):
-    best_bid = get_best_bid(order_book)
-    best_ask = get_best_ask(order_book)
+def get_midpoint_yes(order_book):
+    best_bid_yes = get_best_bid_yes(order_book)
+    best_ask_yes = get_best_ask_yes(order_book)
 
-    if not best_bid or not best_ask:
+    if not best_bid_yes or not best_ask_yes:
         return None
 
-    return round((best_bid + best_ask) / 2, 4)
+    return math.floor(round((best_bid_yes + best_ask_yes) / 2, 4) * 100) / 100
+
+def get_midpoint_no(order_book):
+    best_bid_no = get_best_bid_no(order_book)
+    best_ask_no = get_best_ask_no(order_book)
+
+    if not best_bid_no or not best_ask_no:
+        return None
+
+    return math.floor(round((best_bid_no + best_ask_no) / 2, 4) * 100) / 100
